e9ab5bcd08a8feefe7451dc2ac33ef746a64915d
kunlun2015 <725767227@qq.com>
2016-05-20 20:55:52 +0800

    文件排除

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-    'site_url' => 'http://localhost/amos/git/mysystem/',
+    'site_url' => 'http://localhost/personal/mysystem/',
     'SHOW_PAGE_TRACE' =>true,
 
     'URL_MODEL' => 2,
@@ -15,7 +15,7 @@ return array(
     'DB_HOST'   => 'localhost', // 服务器地址
     'DB_NAME'   => 'my_system', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '123@wepartner', // 密码
+    'DB_PWD'    => '123456', // 密码
     'DB_PORT'   => 3306, // 端口
     'DB_PREFIX' => 'kl_', // 数据库表前缀 
     'DB_CHARSET'=> 'utf8', // 字符集
